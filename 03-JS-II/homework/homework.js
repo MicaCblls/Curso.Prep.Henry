9564e241b567b0b0c4886ca5332f422117b97e43
Micaela Ceballos <micaelaceballos411@gmail.com>
2022-06-30 18:03:16 -0300

    Homework JSII solved

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -165,9 +165,7 @@ function esVerdadero(valor) {
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 
-  if (valor === true) {
-    return "Soy verdadero";
-  } else return "Soy falso";
+  return valor === true ? "Soy verdadero" : "Soy falso";
 }
 
 function tablaDelSeis() {
